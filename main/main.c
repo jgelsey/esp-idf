47d67f0b8c8cedd81827733a87f51b407395f902
Jon <github@gelsey.com>
2022-12-11 21:24:51 -0800

    refactored for higher performance and less memory

--- main/main.c
+++ main/main.c
@@ -34,15 +34,15 @@
 static uint8_t s_led_state = 0;
 // uint32_t reading;
 // uint32_t voltage;
-char voltage_txt[5];
+// char voltage_txt[5];
 
-#define NUMBER_OF_SAMPLES 2048
-uint16_t voltage[NUMBER_OF_SAMPLES];
+// #define NUMBER_OF_SAMPLES 2048
+// uint16_t voltage[NUMBER_OF_SAMPLES];
 // uint16_t voltage_raw[NUMBER_OF_SAMPLES];
-uint16_t voltage_copy[NUMBER_OF_SAMPLES];
-int32_t timestamp[NUMBER_OF_SAMPLES];
+// uint16_t voltage_copy[NUMBER_OF_SAMPLES];
+// int32_t timestamp[NUMBER_OF_SAMPLES];
 
-char Vstr[NUMBER_OF_SAMPLES*10]; //leave lots of room - 10 characters for each entry
+// char Vstr[NUMBER_OF_SAMPLES*10]; //leave lots of room - 10 characters for each entry
 // char Vstr_raw[NUMBER_OF_SAMPLES];
 
 static const char *TAG = "example";
@@ -567,11 +567,11 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
         adc_pattern[i].unit = unit;
         adc_pattern[i].bit_width = SOC_ADC_DIGI_MAX_BITWIDTH;
 
-        // ESP_LOGI(TAG, "adc_pattern[%d].atten is :%x", i, adc_pattern[i].atten);
-        // ESP_LOGI(TAG, "adc_pattern[%d].channel is :%x", i, adc_pattern[i].channel);
-        // ESP_LOGI(TAG, "adc_pattern[%d].unit is :%x", i, adc_pattern[i].unit);
+    //     // ESP_LOGI(TAG, "adc_pattern[%d].atten is :%x", i, adc_pattern[i].atten);
+    //     // ESP_LOGI(TAG, "adc_pattern[%d].channel is :%x", i, adc_pattern[i].channel);
+    //     // ESP_LOGI(TAG, "adc_pattern[%d].unit is :%x", i, adc_pattern[i].unit);
 
-        // ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
+    //     // ESP_LOGI(TAG, "calibration scheme version is %s", "Line Fitting");
         adc_cali_line_fitting_config_t cali_config = {
             .unit_id = unit,
             .atten = ADC_ATTEN_DB_0,
@@ -589,31 +589,36 @@ static void continuous_adc_init(adc_channel_t *channel, uint8_t channel_num, adc
     *out_handle = handle;
     *cali_handle = c_handle;
 
-    bool do_calibration1 = example_adc_calibration_init(ADC_UNIT_1, ADC_ATTEN_DB_0, &adc1_cali_handle);
-}
+    // bool do_calibration1 = ;
+    example_adc_calibration_init(ADC_UNIT_1, ADC_ATTEN_DB_0, &adc1_cali_handle);
+};
 
 // ** finish setup for adc_continuous
 //
 //
 // ************
-int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
+int * get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
 
     esp_err_t ret;
     uint32_t ret_num = 0;
     uint8_t result[EXAMPLE_READ_LEN];
-    memset(result, 0xcc, EXAMPLE_READ_LEN);
-    int i=0,j=0,V=0;
-    uint16_t Vsample=0;
+    // memset(result, 0xcc, EXAMPLE_READ_LEN);
+    int i=0;
+    int V=0;
+    // uint16_t Vsample=0;
     // uint16_t Vrawsample=0;
-    uint16_t Vout;
+    // uint16_t Vout;
+    int Vave=0;
+    long long int Vsquared=0, Vsquared_mean=0, Vsum=0;
+    static int Vret[2];
 
-    uint16_t least_voltage,peak_voltage,median_voltage;
+    // uint16_t least_voltage,peak_voltage,median_voltage;
     // ESP_LOGI(TAG, "Starting get_Vrms...");
 
-    int samples=0;
+    // int samples=0;
 
-    memset(voltage,0,NUMBER_OF_SAMPLES*sizeof(voltage[0]));
-    memset(voltage_copy,0,NUMBER_OF_SAMPLES*sizeof(voltage_copy[0]));
+    // memset(voltage,0,NUMBER_OF_SAMPLES*sizeof(voltage[0]));
+    // memset(voltage_copy,0,NUMBER_OF_SAMPLES*sizeof(voltage_copy[0]));
     // memset(voltage_raw,0,NUMBER_OF_SAMPLES*sizeof(voltage_raw[0]));
 
     s_task_handle = xTaskGetCurrentTaskHandle();
@@ -649,9 +654,9 @@ int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
         ret = adc_continuous_read(handle, result, EXAMPLE_READ_LEN, &ret_num, 0);
         if (ret == ESP_OK) {
             // ESP_LOGI("TASK", "ret is %x, ret_num is %"PRIu32, ret, ret_num);
-            V=0;
-            int Vr=0;
-            Vstr[0]='\0';
+            // V=0;
+            // int Vr=0;
+            // Vstr[0]='\0';
             // Vstr_raw[0]='\0';
 
             // for (i=0;i<0xfff;i++) {
@@ -659,30 +664,48 @@ int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
             //     ESP_LOGI(TAG, "adc_cali_raw_to_voltage:  input:%x  output %x",i,Vr);
             // };
 
-
-            for (int i = 0;i < ret_num;i += SOC_ADC_DIGI_RESULT_BYTES) {
+            int samples=0;
+            for (i = 0;i < ret_num;i += SOC_ADC_DIGI_RESULT_BYTES) {
                 adc_digi_output_data_t *p = (void*)&result[i];
                 // ESP_LOGI(TAG, "Unit: %d, Channel: %d, Value: %x", 1, p->type1.channel, p->type1.data);
                 // ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, (int) p->type1.data, &Vout));
-                Vout=p->type1.data;
-                ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, Vout, &Vr));
+                // Vout=p->type1.data;
+
+                ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, p->type1.data, &V));
+                // Vsquared+=(V-Vave)*(V-Vave);
+                Vsum+=V;
+
                 // ESP_LOGI(TAG, "ADC%d Channel[%d] Cali Voltage: %d mV", ADC_UNIT_1 + 1, EXAMPLE_ADC1_CHAN0, voltage[0][0]);
-                // ESP_LOGI(TAG, "%i: p->type1.data:%i V:%i  Vr:%i",i,(int) p->type1.data, V,Vr);
-                voltage[samples]=Vr;
-                voltage_copy[samples]=Vr;
+                // ESP_LOGI(TAG, "%i: p->type1.data:%i V:%i ",i,(int) p->type1.data, V);
+                // voltage[samples]=Vr;
+                // voltage_copy[samples]=Vr;
 
                 // Vr+= Vout;
-                if (p->type1.data > 0xfff) ESP_LOGI(TAG,"p->type1.data too big: %i",p->type1.data);
+                // if (p->type1.data > 0xfff) ESP_LOGI(TAG,"p->type1.data too big: %i",p->type1.data);
                 // if ((samples % 1000)==0) ESP_LOGI(TAG, "p->type1.data:%i Vout:%i",(int) p->type1.data, Vout);
 
                 samples++;
             }
+            Vave=(samples>0)?Vsum/samples:0;
+            samples=0;
+            for (i = 0;i < ret_num;i += SOC_ADC_DIGI_RESULT_BYTES) {
+                adc_digi_output_data_t *p = (void*)&result[i];
+                // ESP_LOGI(TAG, "Unit: %d, Channel: %d, Value: %x", 1, p->type1.channel, p->type1.data);
+                // ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, (int) p->type1.data, &Vout));
+                // Vout=p->type1.data;
+
+                ESP_ERROR_CHECK(adc_cali_raw_to_voltage(cali_handle, p->type1.data, &V));
+                Vsquared+=(V-Vave)*(V-Vave);
+                samples++;
+            }
+            Vsquared_mean=(samples>0)?Vsquared/samples:0;
+            
             // ESP_LOGI(TAG,"number of samples:%i  V:%i",samples,V); 
 
             // if (samples>0) {
             //     Vsample=(int)(V/samples);  // Average value of the sampled voltage (sampled at 20KHz (or more!)
                 // Vrawsample=(int)(Vr/samples);
-                samples=0;
+                // samples=0;
                 // ESP_LOGI(TAG,"%i: Vrawsample:%i",j,Vrawsample);
             // }
             /**
@@ -708,7 +731,7 @@ int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
     //     j++;
     // }
 
-    int number_of_samples=EXAMPLE_READ_LEN;
+    // int number_of_samples=EXAMPLE_READ_LEN;
 
     // ESP_ERROR_CHECK(adc_continuous_stop(handle));
     // ESP_ERROR_CHECK(adc_continuous_deinit(handle));
@@ -720,23 +743,23 @@ int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
     
     // if (j>NUMBER_OF_SAMPLES) ESP_LOGI(TAG,"j > NUMBER_OF_SAMPLES:%i\n",j);
 
-    memset(Vstr,'\0',sizeof(Vstr)*sizeof(Vstr[0]));
+    // memset(Vstr,'\0',sizeof(Vstr)*sizeof(Vstr[0]));
     // Vstr[0]='\0';
 
     // for (i=0;i<NUMBER_OF_SAMPLES;i++) {
     //     ESP_LOGI(TAG,"before sprintf: voltage[%i]:%x    voltage_copy[%i]:%x",i,voltage[i],i,voltage_copy[i]);
     //     }
 
-    for (i=0;i<number_of_samples;i++) {
-        sprintf(Vstr+strlen(Vstr),"%x,",(unsigned int)voltage[i]);
-        // ESP_LOGI(TAG,"voltage[%i]:%x",i,(int)voltage[i]);
-    };
+    // for (i=0;i<number_of_samples;i++) {
+    //     sprintf(Vstr+strlen(Vstr),"%x,",(unsigned int)voltage[i]);
+    //     // ESP_LOGI(TAG,"voltage[%i]:%x",i,(int)voltage[i]);
+    // };
 
     // for (i=0;i<NUMBER_OF_SAMPLES;i++) {
     //     ESP_LOGI(TAG,"after sprintf: voltage[%i]:%x    voltage_copy[%i]:%x",i,voltage[i],i,voltage_copy[i]);
     //     };
 
-    int n=0;
+    // int n=0;
     // for (i=0;i<strlen(Vstr);i++) {if(Vstr[i]==',') n++;}
     // ESP_LOGI(TAG,"elements:%i voltage:%s\n",n,Vstr);
 
@@ -751,7 +774,7 @@ int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
     // ESP_LOGI(TAG,"elements:%i voltage_copy:%s\n",n,Vstr);
 
 
-    n=0;
+    // n=0;
     // Vstr_raw[0]='\0';
     // for (i=0;i<number_of_samples;i++) {
     //     sprintf(Vstr_raw+strlen(Vstr_raw),"%x,",0xfff&voltage_raw[i]);
@@ -763,44 +786,46 @@ int get_Vrms(adc_continuous_handle_t handle, adc_cali_handle_t cali_handle) {
 
     // ESP_LOGI(TAG,"voltage_copy:%i, (voltage_copy, sizeof(voltage_copy)/sizeof(voltage_copy[0]):%i, sizeof(voltage_copy[0]):%i",sizeof(voltage_copy), sizeof(voltage_copy)/sizeof(voltage_copy[0]), sizeof(voltage_copy[0]));  
 
-    Vstr[0]='\0';
-    sprintf(Vstr+strlen(Vstr),"\nmS,mV\n");
-    for (i=0;i<number_of_samples;i++) {
-        sprintf(Vstr+strlen(Vstr),"%i,%i\n",i,voltage_copy[i]);
-    };
-    n=0;
-    for (i=0;i<strlen(Vstr);i++) {if(Vstr[i]==',') n++;}
-    ESP_LOGI(TAG,"elements:%i in not yet qsorted voltage_copy:%s\n",n,Vstr);
+    // Vstr[0]='\0';
+    // sprintf(Vstr+strlen(Vstr),"\nmS,mV\n");
+    // for (i=0;i<number_of_samples;i++) {
+    //     sprintf(Vstr+strlen(Vstr),"%i,%i\n",i,voltage_copy[i]);
+    // };
+    // n=0;
+    // for (i=0;i<strlen(Vstr);i++) {if(Vstr[i]==',') n++;}
+    // ESP_LOGI(TAG,"elements:%i in not yet qsorted voltage_copy:%s\n",n,Vstr);
 
-    // ESP_LOGI(TAG,"Start qsort");
-    qsort(voltage_copy, sizeof(voltage_copy)/sizeof(voltage_copy[0]), sizeof(voltage_copy[0]), compare);  // sort the voltage array to get voltage bounds
-    // ESP_LOGI(TAG,"Finish qsort");
-    least_voltage=voltage_copy[0];
-    peak_voltage=voltage_copy[number_of_samples-1];
-    median_voltage=voltage_copy[number_of_samples>>1];  // counter/2
-    ESP_LOGW(TAG, "median_voltage:%i  least_voltage:%i  peak_voltage:%i",median_voltage, least_voltage, peak_voltage);
-    Vstr[0]='\0';for (i=0;i<number_of_samples;i++) {sprintf(Vstr+strlen(Vstr),"%x,",voltage_copy[i]);}; ESP_LOGI(TAG,"qsorted voltage_copy :%s",Vstr);
+    // // ESP_LOGI(TAG,"Start qsort");
+    // qsort(voltage_copy, sizeof(voltage_copy)/sizeof(voltage_copy[0]), sizeof(voltage_copy[0]), compare);  // sort the voltage array to get voltage bounds
+    // // ESP_LOGI(TAG,"Finish qsort");
+    // least_voltage=voltage_copy[0];
+    // peak_voltage=voltage_copy[number_of_samples-1];
+    // median_voltage=voltage_copy[number_of_samples>>1];  // counter/2
+    // ESP_LOGW(TAG, "median_voltage:%i  least_voltage:%i  peak_voltage:%i",median_voltage, least_voltage, peak_voltage);
+    // Vstr[0]='\0';for (i=0;i<number_of_samples;i++) {sprintf(Vstr+strlen(Vstr),"%x,",voltage_copy[i]);}; ESP_LOGI(TAG,"qsorted voltage_copy :%s",Vstr);
 
     // calculate RMS
-    int Vac=0; //,bitflip=2;
-    long long int Vrms=0;
-    int Vave=0;
-    for (i=0;i<number_of_samples;i++) {
-        Vac=((int)voltage[i]&0xfff)-median_voltage;
-        Vrms+= Vac*Vac;
-        // ESP_LOGI(TAG,"Vac[%i]:%i    sum(Vrms):%lli  voltage[i]:%i    median_voltage:%i",i,Vac,Vrms,voltage[i]&0xfff,median_voltage);
-        Vave+=(Vac+median_voltage);
-        // if (Vac*(voltage[i-1]-median_voltage)<0) {bitflip++;}
-        // ESP_LOGI("Voltage,  ","Bitflip:%i",bitflip);
-    }
+    // int Vac=0; //,bitflip=2;
+    // long long int Vrms=0;
+    // int Vave=0;
+    // for (i=0;i<number_of_samples;i++) {
+    //     Vac=((int)voltage[i]&0xfff)-median_voltage;
+    //     Vrms+= Vac*Vac;
+    //     // ESP_LOGI(TAG,"Vac[%i]:%i    sum(Vrms):%lli  voltage[i]:%i    median_voltage:%i",i,Vac,Vrms,voltage[i]&0xfff,median_voltage);
+    //     Vave+=(Vac+median_voltage);
+    //     // if (Vac*(voltage[i-1]-median_voltage)<0) {bitflip++;}
+    //     // ESP_LOGI("Voltage,  ","Bitflip:%i",bitflip);
+    // }
     // ESP_LOGI(TAG,"Sum of Vac squared:%lli   number_of_samples: %i",Vrms,number_of_samples);
     // ESP_LOGI(TAG,"Sum of Vac squared/number_of_samples: %lli",(long long int)(Vrms/number_of_samples));
     // ESP_LOGI(TAG,"sqrt(Vac squared/number_of_samples): %f",sqrt(Vrms/number_of_samples));
-    Vrms=(int)sqrt(Vrms/number_of_samples); // root of mean of voltages squared
+    // Vrms=(int)sqrt(Vrms/number_of_samples); // root of mean of voltages squared
 
-    Vave=Vave/number_of_samples;
-    ESP_LOGW(TAG,"\033[1;35m Vave:%i (%x) Vave:%.2f",Vave,Vave,(double)Vave/0xfff*4.1);
-    return Vrms;
+    // Vave=Vave/number_of_samples;
+    // ESP_LOGW(TAG,"\033[1;35m Vave:%lli (%llx) Vave:%.2f",Vave,Vave,(double)Vave/0xfff*4.1);
+    Vret[0]=(int)sqrt(Vsquared_mean);
+    Vret[1]=Vave;
+    return Vret;
 }
 // ************
 httpd_handle_t start_wifi_and_webserver() {
@@ -830,7 +855,7 @@ httpd_handle_t start_wifi_and_webserver() {
     ESP_LOGI(TAG, "Started webserver!!!");
     return server;
 }
-// ************
+// ************ 
 void app_main(void)
 {
     // start_wifi_and_webserver();
@@ -843,7 +868,7 @@ void app_main(void)
     gpio_set_level(GPIO_PIN, false);
     static uint8_t s_led_state = 0;
 
-    int Vrms = 0;
+    int Vrms = 0, Vave=0;
 
     // start loop 
     // const TickType_t xDelay = 500;// / portTICK_PERIOD_MS;
@@ -858,16 +883,19 @@ void app_main(void)
     ESP_ERROR_CHECK(adc_continuous_register_event_callbacks(handle, &cbs, NULL));
     ESP_ERROR_CHECK(adc_continuous_start(handle));
 
+    int *Vret;
 
     while (1) {
-        Vrms=get_Vrms(handle, cali_handle);
-        ESP_LOGW(TAG,"\033[1;34m Vrms:%d (%x)  Vrms:%f\n",Vrms,Vrms,((double)Vrms/0xfff)*4.1);
+        Vret=get_Vrms(handle, cali_handle);
+        Vrms=Vret[0];
+        Vave=Vret[1];
+        ESP_LOGW(TAG,"\033[1;34m Vrms:%.3f   Vave:%.3f",((float)Vrms/0xfff)*4.1,((float)Vave/0xfff)*4.1);
         // Toggle the LED state 
         // ESP_LOGI(TAG, "Turning the LED %s!", s_led_state == true ? "ON" : "OFF");
         gpio_set_level(GPIO_PIN, s_led_state); 
         // vTaskDelay(xDelay);
     }
-    // ESP_ERROR_CHECK(adc_continuous_stop(handle));
-    // ESP_ERROR_CHECK(adc_continuous_deinit(handle));
+    ESP_ERROR_CHECK(adc_continuous_stop(handle));
+    ESP_ERROR_CHECK(adc_continuous_deinit(handle));
 
 }
